5161422f0e089247951dead41ca1ffc5a0323fdc
fds <fds>
2019-10-07 13:07:27 +0200

    simplify (remove) scheduler
    re-enable skiplist
    
    also redirect stderr

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,21 @@ using NewPkgEval
 using Test
 
 v = NewPkgEval.build_julia("v1.2.0")
-pkgs = NewPkgEval.read_pkgs(["Crayons", "Example"])
-dg = NewPkgEval.PkgDepGraph(pkgs, v)
-results = NewPkgEval.run(dg, 1, v)
-@test results["Example"] == :ok
-@test results["Crayons"] == :ok
-results = NewPkgEval.run(dg, 1, v; time_limit = 0.1)
-@test results["Example"] == :fail
-@test results["Crayons"] == :fail
+pkgnames = ["JSON", "TimerOutputs", "Crayons", "Example"]
+pkgs = NewPkgEval.read_pkgs(pkgnames)
+results = NewPkgEval.run(pkgs, 2, v)
+for pkg in pkgnames
+    @test results[pkg] == :ok
+    output = read(joinpath(@__DIR__, "..", logs, "logs-"*string(v), pkg*".log"), String)
+    @test occursin("Testing $pkg tests passed", output)
+end
+results = NewPkgEval.run(pkgs, 2, v; time_limit = 0.1)
+for pkg in pkgnames
+    @test results[pkg] == :fail
+end
+
+mktemp() do path, io
+    NewPkgEval.run_sandboxed_julia(`-e 'print(1337)'`; ver=v"1.1.1", stdout=io)
+    close(io)
+    @test read(path, String) == "1337"
+end
