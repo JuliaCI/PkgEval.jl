ca55236b518ce0edbcec4e5ce990e7000492c60a
Tim Besard <tim.besard@gmail.com>
2019-11-20 11:04:06 +0100

    Default to testing the current version, test both on CI.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,39 @@
 using NewPkgEval
 using Test
 
-v = NewPkgEval.build_julia("v1.2.0")
-
-pkgnames = ["JSON", "TimerOutputs", "Crayons", "Example"]
-pkgs = NewPkgEval.read_pkgs(pkgnames)
-results = NewPkgEval.run(pkgs, 2, v)
-for pkg in pkgnames
-    @test results[pkg] == :ok
-    output = read(joinpath(@__DIR__, "..", logs, "logs-"*string(v), pkg*".log"), String)
-    @test occursin("Testing $pkg tests passed", output)
-end
-results = NewPkgEval.run(pkgs, 2, v; time_limit = 0.1)
-for pkg in pkgnames
-    @test results[pkg] == :fail
+# determine the version to use
+const verstr = get(ENV, "JULIA_VERSION", string(VERSION))
+const v = try
+    ver = VersionNumber(verstr)
+    NewPkgEval.obtain_julia(ver)
+    ver
+catch
+    # either the string is an invalid version number (assume it is a git ref),
+    # or we couldn't obtain this version
+    NewPkgEval.build_julia(verstr)
 end
 
-mktemp() do path, io
-    NewPkgEval.run_sandboxed_julia(`-e 'print(1337)'`; ver=v"1.1.1", stdout=io)
-    close(io)
-    @test read(path, String) == "1337"
+@testset "sandbox" begin
+    mktemp() do path, io
+        NewPkgEval.run_sandboxed_julia(`-e 'print(1337)'`; ver=v, stdout=io)
+        close(io)
+        @test read(path, String) == "1337"
+    end
+end
+
+@testset "PkgEval" begin
+    pkgnames = ["JSON", "TimerOutputs", "Crayons", "Example"]
+    pkgs = NewPkgEval.read_pkgs(pkgnames)
+
+    results = NewPkgEval.run(pkgs, 2, v)
+    for pkg in pkgnames
+        @test results[pkg] == :ok
+        output = read(joinpath(@__DIR__, "..", logs, "logs-"*string(v), pkg*".log"), String)
+        @test occursin("Testing $pkg tests passed", output)
+    end
+
+    results = NewPkgEval.run(pkgs, 2, v; time_limit = 0.1)
+    for pkg in pkgnames
+        @test results[pkg] == :fail
+    end
 end
