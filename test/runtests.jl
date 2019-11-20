26edebe163745d2d2c74824c719ff4e9a983cf30
Tim Besard <tim.besard@gmail.com>
2019-11-20 12:48:26 +0100

    Switch test order to have logs afterwards.

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,15 +25,15 @@ end
     pkgnames = ["JSON", "TimerOutputs", "Crayons", "Example"]
     pkgs = NewPkgEval.read_pkgs(pkgnames)
 
-    results = NewPkgEval.run(pkgs, 2, v)
-    for pkg in pkgnames
-        @test results[pkg] == :ok
-        output = read(joinpath(@__DIR__, "..", logs, "logs-"*string(v), pkg*".log"), String)
-        @test occursin("Testing $pkg tests passed", output)
-    end
-
     results = NewPkgEval.run(pkgs, 2, v; time_limit = 0.1)
     for pkg in pkgnames
         @test results[pkg] == :fail
+    end
+
+    results = NewPkgEval.run(pkgs, 2, v)
+    for pkg in pkgnames
+        @test results[pkg] == :ok
+        output = read(joinpath(NewPkgEval.log_path(v), "$pkg.log"), String)
+        @test occursin("Testing $pkg tests passed", output)
     end
 end
