1b303446325d108253ed9a667365df719769bfc2
Tim Besard <tim.besard@gmail.com>
2020-11-04 07:51:25 +0100

    Adapt to changes in Downloads.jl.

--- src/julia.jl
+++ src/julia.jl
@@ -301,9 +301,8 @@ function obtain_julia_build(spec::String="master", repo_name::String="JuliaLang/
 
         return version
     catch ex
-        # assume this was a download failure, and proceed to build Julia ourselves
-        # TODO: check this was actually a 404
-        isa(ex, ErrorException) || rethrow()
+        # if this was a download failure, proceed to build Julia ourselves
+        isa(ex, Downloads.RequestError) || rethrow()
         bt = catch_backtrace()
         @error "Could not download Julia $spec (version $version), performing a build" exception=(ex,bt)
         perform_julia_build(spec, repo_name)
