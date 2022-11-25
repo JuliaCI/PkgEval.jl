f995022c48d9274fd35aef7d6a1a7f711ef079e6
Tim Besard <tim.besard@gmail.com>
2022-11-25 22:26:28 +0100

    Treat untestable/uninstallable packages as skipped.

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -31,8 +31,6 @@ const reasons = [
     :syntax                 => "package has syntax issues",
     :uncompilable           => "compilation of the package failed",
     :test_failures          => "package has test failures",
-    :untestable             => "package does not have any tests",
-    :unsatisfiable          => "package could not be installed",
     :binary_dependency      => "package requires a missing binary dependency",
     :missing_dependency     => "package is missing a package dependency",
     :missing_package        => "package is using an unknown package",
@@ -43,6 +41,8 @@ const reasons = [
     :time_limit             => "test duration exceeded the time limit",
     :log_limit              => "test log exceeded the size limit",
     # skip
+    :untestable             => "package does not have any tests",
+    :unsatisfiable          => "package could not be installed",
     :unsupported            => "package is not supported by this Julia version",
     :jll                    => "package is a untestable wrapper package",
     :explicit               => "package was blacklisted",
@@ -372,6 +372,15 @@ function evaluate_test(config::Configuration, pkg::Package; use_cache::Bool=true
 
     # log the status and determine a more accurate reason from the log
     @assert status in [:ok, :fail, :kill]
+    ## special cases that indicated we couldn't test at all
+    if occursin("Unsatisfiable requirements detected for package", log)
+        # NOTE: might be the package itself, or one of its dependencies
+        status = :skip
+        reason = :unsatisfiable
+    elseif occursin("Package $(pkg.name) did not provide a `test/runtests.jl` file", log)
+        status = :skip
+        reason = :untestable
+    end
     ## crashes so bad we override the status
     if status !== :kill
         # ... but not in the case of a kill, as a badly-timed signal may cause crashes
@@ -398,12 +407,7 @@ function evaluate_test(config::Configuration, pkg::Package; use_cache::Bool=true
     if status === :fail
         log *= "PkgEval failed"
 
-        reason = if occursin("Unsatisfiable requirements detected for package", log)
-            # NOTE: might be the package itself, or one of its dependencies
-            :unsatisfiable
-        elseif occursin("Package $(pkg.name) did not provide a `test/runtests.jl` file", log)
-            :untestable
-        elseif occursin("cannot open shared object file: No such file or directory", log)
+        reason = if occursin("cannot open shared object file: No such file or directory", log)
             :binary_dependency
         elseif occursin(r"Package .+ does not have .+ in its dependencies", log)
             :missing_dependency
@@ -428,6 +432,8 @@ function evaluate_test(config::Configuration, pkg::Package; use_cache::Bool=true
         log *= "PkgEval terminated"
     elseif status === :crash
         log *= "PkgEval crashed"
+    elseif status === :skip
+        log *= "PkgEval skipped"
     elseif status === :ok
         log *= "PkgEval succeeded"
     end
