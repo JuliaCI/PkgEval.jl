1a796299525977f6e6f2f349d5335a93026caa66
Tim Besard <tim.besard@gmail.com>
2024-01-10 10:09:30 +0100

    Add nothing check to PID name check.

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -161,7 +161,8 @@ function evaluate_script(config::Configuration, script::String, args=``;
 
         # first, send SIGUSR1 to all Julia processes to trigger a profile dump
         for pid in reverse(process_tree(proc))
-            if startswith(pid_comm(pid), "julia")
+            name = pid_comm(pid)
+            if name !== nothing && startswith(name, "julia")
                 pid_kill(pid, #=SIGUSR1=# 10)
                 sleep(10)
             end
