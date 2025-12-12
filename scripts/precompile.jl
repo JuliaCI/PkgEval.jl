d8fad82c325b458a763108b0422b871a2c4224fd
Dilum Aluthge <dilum@aluthge.com>
2025-12-12 02:14:43 -0500

    `scripts/precompile.jl`: Support `Pkg.DEFAULT_IO` being a scoped value (#296)

--- scripts/precompile.jl
+++ scripts/precompile.jl
@@ -1,13 +1,33 @@
 include("common.jl")
 
+function suppress_pkg_output(f::Function)
+    # Need to handle https://github.com/JuliaLang/Pkg.jl/pull/4499,
+    # but also need to handle older Julia versions without ScopedValues
+    use_scoped_values = isdefined(Base, :ScopedValues) && (Pkg.DEFAULT_IO isa Base.ScopedValues.ScopedValue)
+    if !use_scoped_values
+        Pkg.DEFAULT_IO[] = devnull
+    end
+    try
+        if use_scoped_values
+            # Avoid using the @with macro here
+            Base.ScopedValues.with(Pkg.DEFAULT_IO => devnull) do
+                f()
+            end
+        else
+            f()
+        end
+    finally
+        if !use_scoped_values
+            Pkg.DEFAULT_IO[] = nothing
+        end
+    end
+end
+
 config = eval(Meta.parse(ARGS[1]))
 pkg = eval(Meta.parse(ARGS[2]))
 
-try
-    Pkg.DEFAULT_IO[] = devnull
+suppress_pkg_output() do
     Pkg.activate("pkgeval"; shared=true)
-finally
-    Pkg.DEFAULT_IO[] = nothing
 end
 
 # precompile PkgEval run-time dependencies
@@ -19,18 +39,18 @@ if config.goal === :test
     # try to use TestEnv to precompile the package test dependencies
     try
         using TestEnv
-        Pkg.DEFAULT_IO[] = devnull
-        Pkg.activate()
-        TestEnv.activate(pkg.name)
+        suppress_pkg_output() do
+            Pkg.activate()
+            TestEnv.activate(pkg.name)
+        end
     catch err
         @error "Failed to use TestEnv.jl; test dependencies will not be precompiled" exception=(err, catch_backtrace())
         Pkg.activate()
-        Pkg.DEFAULT_IO[] = nothing
     end
 else
-    Pkg.DEFAULT_IO[] = devnull
-    Pkg.activate()
-    Pkg.DEFAULT_IO[] = nothing
+    suppress_pkg_output() do
+        Pkg.activate()
+    end
 end
 
 println("Precompiling package dependencies...")
